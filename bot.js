b7d6ec6904d7a9db1fedc6bdb088bae39fd85415
Mohamed Padr <mohamedpadr94@gmail.com>
2018-10-02 05:06:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ client.on("guildMemberAdd", member => {
 
 client.on('ready', function(){//npm i ms 
     var ms = 5000 ;
-    var setGame = [`Soon`,`iiTzEnd | ● ØF ●#0920`];
+    var setGame = [`Soon`,`Users! [ " ${client.users.size} " ]`,`servers! [ " ${client.guilds.size} " ]`];
     var i = -1;
     var j = 0;
     setInterval(function (){
