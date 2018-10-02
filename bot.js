0a9063ad983189b7c8bc6ca2130055b988400686
Mohamed Padr <mohamedpadr94@gmail.com>
2018-10-02 06:49:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -667,6 +667,18 @@ message.channel.send({embed:embed});
                             })`
                         }
                         });
+
+                        function timeCon(time) { //bot2
+                            let days = Math.floor(time % 31536000 / 86400)
+                            let hours = Math.floor(time % 31536000 % 86400 / 3600)
+                            let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+                            let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+                            days = days > 9 ? days : '0' + days
+                            hours = hours > 9 ? hours : '0' + hours
+                            minutes = minutes > 9 ? minutes : '0' + minutes
+                            seconds = seconds > 9 ? seconds : '0' + seconds
+                            return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
+                        }
    
 
    
