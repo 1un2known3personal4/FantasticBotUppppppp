1720258f9e308e3bc21a79dce7295b0a4742efde
Mohamed Padr <mohamedpadr94@gmail.com>
2018-10-03 00:32:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7489,7 +7489,47 @@ client.on("message", function(message) {
 });
 
 
+client.on('message', message => { 
+        if(!message.channel.guild) return;
+    if(message.content.startsWith('p!bc')) {
+    if(!message.channel.guild) return message.channel.send('**This is only for servers**').then(m => m.delete(5000));
+    if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**Sorry you do not have permission** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "Fantastic Bot";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**You must type a word or phrase to send broadcast**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
 
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`**☑ | Been sent BroadCast to ${message.guild.members.size} Member**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+    Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setTitle('Broadcast')
+    .addField('Server', message.guild.name)
+    .addField('Sender', message.author.username)
+    .addField('Message', args)
+    .setImage("https://cdn.discordapp.com/attachments/488945684595081217/496810404077371392/broadcast-wheels.png")
+    .setThumbnail(message.author.avatarURL)
+    .setFooter(copy, Rocket.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Canceled BroadCast**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
 
 
 
